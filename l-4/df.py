216660d7e59af8097c2efc815fbe47dc7e1d200d
NoumanAmir657 <noumanamir453@gmail.com>
2023-08-03 21:00:07 +0500

    refactored the code of df.py

--- l-4/df.py
+++ l-4/df.py
@@ -5,11 +5,20 @@ import json
 
 from cfg import form_blocks, block_map, add_terminators, edges
 
-def print_blocks(blocks):
-    for block in blocks:
-        for ins in block:
-            print(ins)
+def print_analysis(blk_map, in_, out_):
+    for block in blk_map:
+        print(block)
+        print('\t', in_[block])
+        print('\t', out_[block])
+        print('\n')
 
+def union(sets):
+    out = set()
+    
+    for s in sets:
+        out.update(s)
+    
+    return out
 
 def worklist_algo(blocks, direction, init, transfer, merge):
     if direction:
@@ -43,60 +52,53 @@ def worklist_algo(blocks, direction, init, transfer, merge):
     else:
         return out_, in_
 
-def union(sets):
-    out = set()
-    
-    for s in sets:
-        out.update(s)
-    
-    return out
-
-def rd_transfer(block, inv):
-    outv = set()
-    outv.update([ins['dest'] for ins in block if 'dest' in ins])
-    outv.update(inv)
-
-    return outv
-
 def rd(func):
     blk_map = block_map((form_blocks(func['instrs'])))
     add_terminators(blk_map)
     
+    def rd_transfer(block, inv):
+        outv = set()
+        outv.update([ins['dest'] for ins in block if 'dest' in ins])
+        outv.update(inv)
+
+        return outv
+
     in_, out_ = worklist_algo(blk_map, True, set(), rd_transfer, union)
+    
+    print_analysis(blk_map, in_, out_)
 
-    for block in blk_map:
-        print(block)
-        print('\t', in_[block])
-        print('\t', out_[block])
-        print('\n')
-
-def live_transfer(block, inv):
-    defined = set()
-    used = set()
-
-    for ins in block:
-        used.update(arg for arg in ins.get('args', []) if arg not in defined)
-        if 'dest' in ins:
-            defined.add(ins['dest'])
-
-
-    return used.union(inv - {ins['dest'] for ins in block if 'dest' in ins})
 
 def live(func):
     blk_map = block_map((form_blocks(func['instrs'])))
     add_terminators(blk_map)
+
+    def live_transfer(block, inv):
+        defined = set()
+        used = set()
+
+        for ins in block:
+            used.update(arg for arg in ins.get('args', []) if arg not in defined)
+            if 'dest' in ins:
+                defined.add(ins['dest'])
+
+
+        return used.union(inv - {ins['dest'] for ins in block if 'dest' in ins})    
     
     in_, out_ = worklist_algo(blk_map, False, set(), live_transfer, union)
 
-    for block in blk_map:
-        print(block)
-        print('\t', in_[block])
-        print('\t', out_[block])
-        print('\n')
+    print_analysis(blk_map, in_, out_)
 
 
 if __name__ == '__main__':
     prog = json.load(sys.stdin)
 
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'define':
+            df = rd
+        elif sys.argv[1] == 'live':
+            df = live
+    else:
+        df = rd
+ 
     for func in prog['functions']:
-        live(func)
+        df(func)
